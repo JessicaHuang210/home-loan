e04bda85b495d114517c6e7bf59d30e78ed16a5f
JessicaHuang210 <jess0210777@gmail.com>
2023-11-24 12:40:47 +0800

    update: 國泰算法

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,14 @@ export default defineNuxtConfig({
       link: [
         { rel: "icon", type: "image/png", href: "/penueling-512x512.png" },
       ],
+      title: "房貸試算 - 本金平均攤還",
+      meta: [
+        {
+          name: " description",
+          content:
+            "房貸試算，房貸試算表，房貸試算表2023，房貸計算器，貸款計算器，房貸計算器2023，貸款試算，本金平均攤還，房貸利息試算，房貸利息計算，房貸每月攤還",
+        },
+      ],
     },
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <UContainer>
     <h1>房貸試算</h1>
-    <p>本金平均攤還，單一利率</p>
+    <p class="text-gray-400 dark:text-gray-500">
+      本金平均攤還，單一利率，計算方式參考國泰世華銀行
+    </p>
     <div class="flex flex-col md:flex-row gap-10 mb-10">
       <div class="md:basis-1/2">
         <UForm :validate="validate" :state="formModel" @submit="handleSubmit">
@@ -25,12 +27,14 @@
               name="rate"
               label="貸款利率(年利率%)"
               class="mb-4 md:basis-1/2"
+              require
             >
               <UInput
                 type="number"
                 placeholder="請輸入貸款利率(年利率%)"
                 v-model="formModel.rate"
                 size="xl"
+                step="0.01"
               >
                 <template #trailing>%</template>
               </UInput>
@@ -55,50 +59,65 @@
       <UCard class="md:basis-1/2">
         <ul class="desc px-4 py-5 sm:p-6 md:basis-1/2">
           <li class="text-green-500">
-            <span class="text-gray-700 dark:text-gray-400">每月還款：</span>
+            <span class="title text-gray-700 dark:text-gray-400"
+              >每月還款：</span
+            >
             {{ states.arr?.[0]?.amount?.toLocaleString("en-US") }}
-            <span class="text-slate-600 dark:text-slate-300"> 元/月</span>
+            <span class="title text-slate-600 dark:text-slate-300">
+              元/月 (遞減)
+            </span>
           </li>
           <li>
-            <span class="text-gray-700 dark:text-gray-400">期數：</span
+            <span class="title text-gray-700 dark:text-gray-400">期數：</span
             >{{ states.period }}期 ({{ formModel.year }}年)
           </li>
           <li>
-            <span class="text-gray-700 dark:text-gray-400">本金：</span>
+            <span class="title text-gray-700 dark:text-gray-400">本金：</span>
             {{ states.principal.toLocaleString("en-US") }}
           </li>
           <li>
-            <span class="text-gray-700 dark:text-gray-400">利息：</span>
+            <span class="title text-gray-700 dark:text-gray-400">利息：</span>
             {{ states.rateAll.toLocaleString("en-US") }}
           </li>
           <li>
-            <span class="text-gray-700 dark:text-gray-400">本利和：</span>
+            <span class="title text-gray-700 dark:text-gray-400">本利和：</span>
             {{ (states.principal + states.rateAll).toLocaleString("en-US") }}
           </li>
         </ul>
       </UCard>
     </div>
-    <UTable
-      :rows="states.arr"
-      :columns="columns"
-      :empty-state="{ label: '暫無資料' }"
-    >
-      <template #period-data="{ row }"> 第 {{ row.period }} 期 </template>
-      <template #principalM-data="{ row }">
-        {{ row.principalM.toLocaleString("en-US") }}
-      </template>
-      <template #rateM-data="{ row }">
-        {{ row.rateM.toLocaleString("en-US") }}
-      </template>
-      <template #amount-data="{ row }">
-        {{ row.amount.toLocaleString("en-US") }}
-      </template>
-      <template #restPrincipal-data="{ row }">
-        {{ row.restPrincipal.toLocaleString("en-US") }}
-      </template>
-    </UTable>
+    <div class="table-scroll" ref="tableScrollRef">
+      <UTable
+        :rows="states.arr"
+        :columns="columns"
+        :empty-state="{ label: '暫無資料' }"
+      >
+        <template #period-data="{ row }"> 第 {{ row.period }} 個月 </template>
+        <template #principalM-data="{ row }">
+          {{ row.principalM.toLocaleString("en-US") }}
+        </template>
+        <template #rateM-data="{ row }">
+          {{ row.rateM.toLocaleString("en-US") }}
+        </template>
+        <template #amount-data="{ row }">
+          <span class="text-lg font-black text-green-500">
+            {{ row.amount.toLocaleString("en-US") }}
+          </span>
+          元
+        </template>
+        <template #restPrincipal-data="{ row }">
+          {{ row.restPrincipal.toLocaleString("en-US") }}
+        </template>
+      </UTable>
+    </div>
+    <div class="flex flex-wrap gap-3 mt-20">
+      <UBadge color="gray" variant="solid" v-for="i in tags" :key="i">
+        #{{ i }}
+      </UBadge>
+    </div>
   </UContainer>
-  <footer class="mt-48 p-4 text-center bg-slate-50 dark:bg-slate-800">
+
+  <footer class="mt-20 p-4 text-center bg-slate-50 dark:bg-slate-800">
     <h6 class="text-gray-400 dark:text-gray-200 text-sm">
       2023
       <ULink to="https://penueling.com" target="_blank"
@@ -118,12 +137,13 @@ const validate = (state) => {
   const errors = [];
   if (!state.totalAmount)
     errors.push({ path: "totalAmount", message: "必填欄位" });
+  if (!state.rate) errors.push({ path: "rate", message: "必填欄位" });
   return errors;
 };
 
 const states = reactive({
   principal: computed(() => formModel.totalAmount * 10000), // 貸款總額
-  principalM: computed(() => Math.round(states.principal / states.period) + 1), //每月償還本金
+  principalM: computed(() => Math.round(states.principal / states.period)), //每月償還本金
   period: computed(() => formModel.year * 12), // 貸款期數
   rateM: computed(() => formModel.rate / 100 / 12), // 貸款期數
   arr: [],
@@ -145,7 +165,7 @@ const columns = ref([
   },
   {
     key: "amount",
-    label: "本利和",
+    label: "每月還款金額",
   },
   {
     key: "restPrincipal",
@@ -160,26 +180,34 @@ const tags = ref([
   "房貸利息計算",
   "房貸每月攤還",
 ]);
+const tableScrollRef = ref(null);
 
 const handleSubmit = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  tableScrollRef.value.scrollTo({ top: 0, behavior: "smooth" });
   const newArr = [];
   for (let i = 0; i < states.period; i++) {
+    const _principalMRound = Math.round(states.principal / states.period); // 四捨五入的每月本金
+    const _principalRoundDeviation =
+      Math.round(states.principal / states.period) * states.period -
+      states.principal; // 四捨五入的每月本金*期數-真正本金(誤差)
+    const __principalMLast = _principalMRound - _principalRoundDeviation;
     const _principalM =
-      states.principal - states.principalM * (i + 1) > 0
-        ? states.principalM
-        : states.principal - states.principalM * i;
+      i < states.period - 1 ? _principalMRound : __principalMLast;
     const _rateM = Math.round(
       (states.principal - states.principalM * i) * states.rateM
     );
+
+    const _restPrincipal = states.principal - states.principalM * (i + 1);
     newArr.push({
       period: i + 1,
       principalM: _principalM,
       rateM: _rateM,
       amount: _principalM + _rateM,
-      restPrincipal: Math.max(
-        states.principal - states.principalM * (i + 1),
-        0
-      ),
+      restPrincipal:
+        i < states.period - 1
+          ? _restPrincipal
+          : _restPrincipal + _principalRoundDeviation,
     });
   }
   states.arr = newArr;
@@ -208,11 +236,15 @@ p {
   margin-bottom: 20px;
   display: flex;
 }
-.desc span {
+.desc span.title {
   display: block;
   min-width: 100px;
   font-size: 15px;
   font-weight: normal;
   letter-spacing: 0;
+}
+.table-scroll {
+  max-height: 500px;
+  overflow-y: auto;
 }
 </style>
